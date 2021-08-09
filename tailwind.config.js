2aebeda1478407e592a2a5fc0e79bbd08beb938b
Can Polat <can.polatt@yahoo.com>
2021-08-09 19:52:50 +0300

    🎉 first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary:'#05CE91',
+        secondary:'#4FC2C2',
+        dimgrey:'#696969',
+      },
+      fontFamily:{
+        body:['Sofia Pro'],
+        header:['Work Sans']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
